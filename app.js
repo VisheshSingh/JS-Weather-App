55e6304fb9d31da1498a31e6e2a726beb74f0084
Vishesh <visheshsingh16@gmail.com>
2018-11-22 13:01:07 -0500

    Change city and update weather

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const weather = new Weather("New York");
 // INIT UI OBJECT
 const ui = new UI();
 
+// Change location event
+document.getElementById("w-change-btn").addEventListener("click", function(e) {
+  const city = document.getElementById("city").value;
+
+  // update the city
+  weather.changeLocation(city);
+
+  // get updated weather
+  getWeather();
+
+  //close modal
+  $("#locModal").modal("hide");
+});
 // Get weather on DOM load
 document.addEventListener("DOMContentLoaded", getWeather);
 
